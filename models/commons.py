e439050c9a05dd718c5d39e9d7caf90027b348cf
Minh Nguyen <comrc019@icloud.com>
2024-05-30 20:21:37 -0700

    na

---
+++ models/commons.py
@@ -0,0 +1,16 @@
+"""
+----------MIMIC-V data paths----------
+"""
+
+diagnose_icd_file_path = '../data/diagnoses_icd.csv'
+patients_file_path = '../data/patients.csv'
+
+# https://www.aapc.com/codes/code-search/
+# ICD-10 code for kidney failure
+kidney_failure_icd_10_codes = ['N17', 'N170', 'N171', 'N172', 'N178', 'N179', 'N19']
+# ICD-9 code for kidney failure
+kidney_failure_icd_9_codes = ['5849']
+
+"""
+----------MIMIC-III data paths----------
+"""
---
+++ models/data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from commons import (
+    diagnose_icd_file_path, kidney_failure_icd_10_codes, kidney_failure_icd_9_codes, patients_file_path)
+def analysis_diagnose_icd():
+    D = pd.read_csv(diagnose_icd_file_path)
+    print(
+        f"number of rows: {D.shape[0]}. number of subjects: {D['subject_id'].nunique()}"
+    )
+
+    print("filtering for kidney failure")
+    k = kidney_failure_icd_10_codes + kidney_failure_icd_9_codes
+    df = D[D['icd_code'].isin(k)]
+    print(
+        f"number of rows: {df.shape[0]}. number of subjects: {df['subject_id'].nunique()}\n"
+        f"percentage of subjects in dataset with kidney failure: {df['subject_id'].nunique() / D['subject_id'].nunique() * 100:.3f}"
+    )
+
+    kf_subjects = df['subject_id'].unique()
+    print(f"number of subjects with kidney failure (for validation): {len(kf_subjects)}")
+
+    df = pd.read_csv(patients_file_path)
+    df = df[df['subject_id'].isin(kf_subjects)]
+
+    print(
+        f"printing number of subjects out for validation\n"
+        f"number of rows: {df.shape[0]}. number of subjects: {df['subject_id'].nunique()}"
+    )
+
+    print("analyzing patients")
+
+    # analyze gender statistics
+    print(
+        f"Gender statistics:\n"
+        f"{df['gender'].value_counts()}."
+    )
+
+if __name__ == '__main__':
+    analysis_diagnose_icd()
