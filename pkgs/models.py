81e5f86291b790d273e8ab2f27546bd74659f118
minhnguyen <comrc019@icloud.com>
2024-07-17 11:05:24 -0700

    na

--- pkgs/models.py
+++ pkgs/models.py
@@ -21,11 +21,29 @@ cph.fit(df, duration_col='duration', event_col='event')
 cph.print_summary()
 
 # Predict the median survival time for each individual
-median_survival_times = cph.predict_median(df)
-print(median_survival_times)
+censored_df = df.loc[~df['event'].astype(bool)]
+M = cph.predict_median(censored_df)
+print(f"predict_median\n{M}")
 
 # Plot the survival functions
 cph.plot()
 
 D = cph.predict_survival_function(df)
-print(D)
+print(f"predict_survival_function\n{D}")
+
+
+# Example new data
+new_data = pd.DataFrame({
+    'age': [60, 40, 50],
+    'treatment': [0, 0, 1],
+    'duration': [5, 6, 6]
+})
+
+# Predict survival function for the new data
+surv_pred = cph.predict_survival_function(new_data)
+
+# Print the predicted survival probabilities
+print(f"predict_survival_function\n{surv_pred}")
+
+M = cph.predict_median(new_data)
+print(f"predict_median\n{M}")
