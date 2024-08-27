2243da386739eff5154815abfc2bb941eee79acd
minhnguyen <comrc019@icloud.com>
2024-08-26 22:06:40 -0700

    na

--- pkgs/data.py
+++ pkgs/data.py
@@ -153,34 +153,30 @@ def extract_num_from_value(value):
         return float('nan')
 
 
-# calculate eGFR using the CKD-EPI formula
+# calculate eGFR using the CKD-EPI 2021 formula
+# https://www.mdcalc.com/calc/3939/ckd-epi-equations-glomerular-filtration-rate-gfr#evidence
+# based on https://pubmed.ncbi.nlm.nih.gov/34554658/
 def calculate_eGFR(row):
-    race = row['race']
-    gender = 'FEMALE' if row['gender'] == 'F' else 'MALE'
+    gender = row['gender']
     age = row['anchor_age']
-    serum_creatinine = row['valuenum']
-    serum_creatinine_unit = row['valueuom']
-
+    serum_creatinine = row['valuenum']  # unit of serum creatinine in MIMIC-IV data is already mg/dL.
     assert serum_creatinine != 0, f"bad value of serum_creatinine {row['subject_id']}"
+    assert gender in ['M', 'F'], \
+        f"bad value of gender {row['subject_id']}"  # range of gender value in mimic-iv dataset.
 
-    # CKD-EPI constants
-    if gender == 'male':
-        k = 0.9
-        alpha = -0.411
-        constant = 1.0
-    else:
-        k = 0.7
-        alpha = -0.329
-        constant = 1.018
+    def get_ab():
+        if gender == 'M':
+            if serum_creatinine <= 0.9:
+                return 0.9, -0.302
+            return 0.9, -1.2
+        if serum_creatinine <= 0.7:
+            return 0.7, -0.241
+        return 0.7, -1.2
 
-    if race == 'black':
-        constant *= 1.159
+    gender_factor = 1 if gender == "M" else 1.012
+    A, B = get_ab()
 
-    # CKD-EPI equation
-    eGFR = 141 * min(serum_creatinine / k, 1) ** alpha * max(serum_creatinine / k,
-                                                             1) ** -1.209 * 0.993 ** age * constant
-
-    return eGFR
+    return 142 * ((serum_creatinine / A) ** B) * (0.9938 ** age) * gender_factor
 
 
 def get_lab_events_for_patients(patient_df):
