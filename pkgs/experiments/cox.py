7c668a5fc79fe9e9e2910e0e2275245548e92e8a
minhnguyen <comrc019@icloud.com>
2024-09-13 15:50:58 -0700

    na

--- pkgs/experiments/cox.py
+++ pkgs/experiments/cox.py
@@ -1,9 +1,9 @@
 from lifelines.utils import concordance_index
 import joblib
-from lifelines import CoxTimeVaryingFitter
+from lifelines import CoxTimeVaryingFitter, CoxPHFitter
 import os
 
-from pkgs.commons import cox_model_path
+from pkgs.commons import tv_cox_model_path
 from pkgs.data.store_model_data import get_train_test_data
 from pkgs.experiments.utils import report_metric
 
@@ -15,20 +15,46 @@ def prep_data(df):
     df.loc[df['start'] == df['stop'], 'stop'] += 1e-5  # Adding a small value to ensure stop > start
     return df
 
-def run_cox_model():
+def run_time_varying_cox_model():
     data_train, data_test = get_train_test_data()
     data_train = prep_data(data_train)[['subject_id', 'start', 'stop', 'has_esrd', 'egfr']]
     data_test = prep_data(data_test)[['subject_id', 'start', 'stop', 'has_esrd', 'egfr']]
 
-    if not os.path.exists(cox_model_path):
+    if not os.path.exists(tv_cox_model_path):
         model = CoxTimeVaryingFitter()
 
         print(f'Fitting model:\n')
         model.fit(data_train, event_col='has_esrd', id_col='subject_id')
 
-        joblib.dump(model, cox_model_path)
+        joblib.dump(model, tv_cox_model_path)
     else:
-        model = joblib.load(cox_model_path)
+        model = joblib.load(tv_cox_model_path)
+
+    print('Evaluate on training data')
+    risk_scores = model.predict_partial_hazard(data_train)
+    c_index = report_metric(concordance_index(data_train['stop'], -risk_scores, data_train['has_esrd']))
+    print(f'Concordance Index: {c_index}')
+
+    print('Evaluate on test data')
+    risk_scores_test = model.predict_partial_hazard(data_test)
+    c_index_test = report_metric(concordance_index(data_test['stop'], -risk_scores_test, data_test['has_esrd']))
+    print(f'Concordance Index Test: {c_index_test}')
+
+
+def run_cox_model():
+    data_train, data_test = get_train_test_data()
+    data_train = prep_data(data_train)[['subject_id', 'start', 'stop', 'has_esrd', 'egfr']]
+    data_test = prep_data(data_test)[['subject_id', 'start', 'stop', 'has_esrd', 'egfr']]
+
+    if not os.path.exists(tv_cox_model_path):
+        model = CoxPHFitter()
+
+        print(f'Fitting model:\n')
+        model.fit(data_train, event_col='has_esrd', id_col='subject_id')
+
+        joblib.dump(model, tv_cox_model_path)
+    else:
+        model = joblib.load(tv_cox_model_path)
 
     print('Evaluate on training data')
     risk_scores = model.predict_partial_hazard(data_train)
@@ -42,5 +68,5 @@ def run_cox_model():
 
 
 if __name__ == '__main__':
-    run_cox_model()
+    run_time_varying_cox_model()
 
