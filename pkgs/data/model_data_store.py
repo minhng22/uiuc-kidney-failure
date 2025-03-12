5822cdbbd1bd4afe55df127213488a6aeef09ba0
minhng22 <mnguyenn@protonmail.com>
2025-03-12 13:28:03 -0700

    na

--- pkgs/data/model_data_store.py
+++ pkgs/data/model_data_store.py
@@ -12,7 +12,7 @@ import pandas as pd
 # Pick a small subset of the data to test the models
 # Random pick censored and uncensored patients.
 def mini(df):
-    num_subjects = 500
+    num_subjects = 2000
 
     esrd_patients = df[df['has_esrd'] == True]['subject_id'].unique()
     non_esrd_patients = df[~df['subject_id'].isin(esrd_patients)]['subject_id'].unique()
