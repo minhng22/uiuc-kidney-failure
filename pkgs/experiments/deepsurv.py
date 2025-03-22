007ba076914a49794369caa2b6743ebb2221b06e
minhng22 <mnguyenn@protonmail.com>
2025-03-22 12:46:45 -0700

    na

--- pkgs/experiments/deepsurv.py
+++ pkgs/experiments/deepsurv.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import torch.optim as optim
 import optuna
 
@@ -7,10 +6,12 @@ from pkgs.models.deepsurv import DeepSurv
 from pkgs.data.model_data_store import get_train_test_data_egfr
 from torch.utils.data import Dataset, DataLoader
 from lifelines.utils import concordance_index
-from pkgs.experiments.utils import report_metric, evaluate_rnn_model
+from pkgs.experiments.utils import evaluate_rnn_model
 
 import os
 from pkgs.commons import egfr_ti_deepsurv_model_path
+
+deep_surv_features = ['egfr']
 
 class DeepSurvDataset(Dataset):
     def __init__(self, df, features, duration_col, event_col):
@@ -42,20 +43,19 @@ def neg_log_partial_likelihood(risk, durations, events):
     return loss
 
 def objective(trial):
-    features = ['egfr']
     duration_col = 'duration_in_days'
     event_col = 'has_esrd'
 
     df, df_test = get_train_test_data_egfr(False)
 
-    train_dataset = DeepSurvDataset(df, features, duration_col, event_col)
+    train_dataset = DeepSurvDataset(df, deep_surv_features, duration_col, event_col)
     
     # use full batch to preven neg log likehood loss crash on no positive datapoint in batch
     train_loader = DataLoader(train_dataset, batch_size=len(train_dataset), shuffle=True)
 
     input_dim = 1
     hidden_dims = [trial.suggest_int(f"hidden_dim_{i}", 16, 256) for i in range(10)]
-    learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
+    learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-2, log=True)
     num_epochs = 25
 
     model = DeepSurv(input_dim, hidden_dims)
@@ -71,67 +71,39 @@ def objective(trial):
             loss.backward()
             optimizer.step()
 
-    X_test = torch.tensor(df_test[features].values, dtype=torch.float32)
+    X_test = torch.tensor(df_test[deep_surv_features].values, dtype=torch.float32)
     model.eval()
     with torch.no_grad():
         test_risk_scores = model(X_test)
 
     c_index = concordance_index(df_test['duration_in_days'], test_risk_scores, df_test['has_esrd'])
+    trial.set_user_attr(key="model", value=model)
     return c_index
 
-def run():
+def ex_optuna():
     study = optuna.create_study(direction='maximize')
     study.optimize(objective, n_trials=100)
 
     print("Number of finished trials: ", len(study.trials))
     print("Best trial:")
     trial = study.best_trial
-    print("  Value: ", trial.value)
-    print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
 
-    features = ['egfr']
-    duration_col = 'duration_in_days'
-    event_col = 'has_esrd'
+    print(f"Best hyperparameters: {study.best_params}")
+    best_model = trial.user_attrs["model"]
 
-    df, df_test = get_train_test_data_egfr(False)
+    return best_model
 
-    train_dataset = DeepSurvDataset(df, features, duration_col, event_col)
-    train_loader = DataLoader(train_dataset, batch_size=len(train_dataset), shuffle=True)
-
-    input_dim = 1
-    hidden_dims = [trial.params[f"hidden_dim_{i}"] for i in range(5)]
-    learning_rate = trial.params['learning_rate']
-    num_epochs = 25
-
-    model = DeepSurv(input_dim, hidden_dims)
+def run():
+    _, df_test = get_train_test_data_egfr(False)
 
     if os.path.exists(egfr_ti_deepsurv_model_path):
         print("Loading from saved weights")
-        model.load_state_dict(torch.load(egfr_ti_deepsurv_model_path, weights_only=True))
+        model = torch.load(egfr_ti_deepsurv_model_path, weights_only=False)
     else:
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        model = ex_optuna()
+        torch.save(model, egfr_ti_deepsurv_model_path)
 
-        model.train()
-        for epoch in range(num_epochs):
-            for batch in train_loader:
-                X_batch, durations_batch, events_batch = batch
-                optimizer.zero_grad()
-                risk_scores = model(X_batch)
-                loss = neg_log_partial_likelihood(risk_scores, durations_batch, events_batch)
-                loss.backward()
-                optimizer.step()
-        torch.save(model.state_dict(), egfr_ti_deepsurv_model_path)
-        print("Training complete.")
-
-    X_test = torch.tensor(df_test[features].values, dtype=torch.float32)
-    model.eval()
-    with torch.no_grad():
-        test_risk_scores = model(X_test)
-
-    c_index = report_metric(concordance_index(df_test['duration_in_days'], test_risk_scores, df_test['has_esrd']))
-    print("C-Index on Test Data:", c_index)
+    evaluate_rnn_model(model, df_test, deep_surv_features)
 
 if __name__ == '__main__':
     run()
