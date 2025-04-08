079f66488091716b4156644a1230fde6f46adb52
minhng22 <comrc019@icloud.com>
2025-04-07 22:44:18 -0500

    na

--- run_rnnsurv.sh
+++ run_rnnsurv.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# Determine the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Run the command in the background using nohup
-nohup python -m pkgs.experiments.rnnsurv > "${SCRIPT_DIR}/rnnsurv.log" 2>&1 &
+nohup python -u -m pkgs.experiments.rnnsurv > "${SCRIPT_DIR}/rnnsurv.log" 2>&1 &
 
-# Save the process ID (PID) to a file within the same directory for later tracking or stopping
 echo $! > "${SCRIPT_DIR}/rnnsurv.pid"
 
 echo "rnnsurv started with PID $(cat ${SCRIPT_DIR}/rnnsurv.pid)"
