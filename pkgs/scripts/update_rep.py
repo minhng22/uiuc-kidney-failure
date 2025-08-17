1914923726cf40053f0f9a38cad2e6d95ee48bc4
minhng22 <comrc019@icloud.com>
2025-08-17 13:44:47 -0500

    na

--- pkgs/scripts/update_rep.py
+++ pkgs/scripts/update_rep.py
@@ -23,8 +23,8 @@ def update_rep_in_commons(rep_num):
         with open(commons_file, 'r') as f:
             content = f.read()
         
-        pattern = r"generate_data_path_latest_rep = f'\{project_dir\(\)\}/generated_data/rep\d+'"
-        replacement = f"generate_data_path_latest_rep = f'{{project_dir()}}/generated_data/rep{rep_num}'"
+        pattern = r"current_rep = \d+"
+        replacement = f"current_rep = {rep_num}"
         
         new_content = re.sub(pattern, replacement, content)
         
