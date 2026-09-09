0321d1b60e6aec8de21a54dc9d6d96c5c2db989e
minhng22 <comrc019@icloud.com>
2026-09-09 07:59:46 -0500

    init

--- pkgs/experiments/srf.py
+++ pkgs/experiments/srf.py
@@ -169,7 +169,6 @@ def run_scenario(scenario: ExperimentScenario):
 
 
 if __name__ == '__main__':
-    run_survival_rf()
     print("\nRunning FOUR_FEATURES Survival RF model evaluation...")
     run_scenario(ExperimentScenario.FOUR_FEATURES)
     print("\nRunning EIGHT_FEATURES Survival RF model evaluation...")
